61546308b7245c634be51a7b38e28988b25a14a0
Darkoh <lukekirk05@gmail.com>
2025-06-21 02:13:23 +0100

    Edited Top Bar and Dark Mode Toggle Layout

--- src/app_setup.rs
+++ src/app_setup.rs
@@ -1,4 +1,4 @@
-use eframe::{egui::{self, CentralPanel, Context, TopBottomPanel}, App, Frame};
+use eframe::{egui::{self, CentralPanel, Context, TopBottomPanel, Vec2}, App, Frame};
 
 // The app
 pub struct GameLog { 
@@ -23,11 +23,21 @@ impl App for GameLog {
                 ctx.set_visuals(egui::Visuals::light());
             }
 
-            let label = if self.dark_mode { "Light Mode" } else { "Dark Mode" };
-            
-            if ui.button(label).clicked() {
-                self.dark_mode = !self.dark_mode;
-            }
+            // Otherwise Text in button isn't centered. This also puts buttons to the right of the Top Bar
+            ui.with_layout(egui::Layout::centered_and_justified(egui::Direction::RightToLeft), |ui| {
+
+                // Purpose of this is so we can have multiple buttons aligned horizontally. Without it, the first button created takes up the whole top bar with no room for the others
+                ui.horizontal(|ui|
+                    {
+                    let label = if self.dark_mode { "Light Mode" } else { "Dark Mode" };
+
+                    if ui.add(egui::Button::new(label)
+                        .min_size(Vec2::new(60.0, 40.0 )))
+                    .clicked() {
+                        self.dark_mode = !self.dark_mode;
+                    }
+                });
+            });
         });
 
         CentralPanel::default().show(ctx, |ui| {
