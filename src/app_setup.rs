49a43ebdb1014fe8b347850ffdc19eb8795e1dc0
Darkoh <lukekirk05@gmail.com>
2025-06-22 02:23:03 +0100

    Added assets to my app. They are loaded into the GPU and turned into usable textures at startup and saved to a vector to be used whenever needed throughout the program

--- src/app_setup.rs
+++ src/app_setup.rs
@@ -1,16 +1,53 @@
-use eframe::{egui::{self, CentralPanel, Context, FontId, RichText, TopBottomPanel, Vec2}, App, Frame};
+use eframe::{egui::{self, CentralPanel, ColorImage, Context, FontId, ImageButton, RichText, TextureHandle, TextureOptions, TopBottomPanel, Vec2}, epaint, App, Frame};
+use image::GenericImageView;
+
 
 // The app
 pub struct GameLog { 
     pub dark_mode: bool, // Attribute for Toggling dark mode
+    pub assets: Vec<egui::TextureHandle>
 }
 
-// Default app settings
-impl Default for GameLog {
-    fn default() -> Self {
-        Self { dark_mode: true }
+// App settings on startup
+impl GameLog {
+    // Constructor to create app and load assets
+    pub fn startup(ctx: &egui::Context)  -> Self {
+        let assets = Self::load_assets_from_bytes(ctx); // Load in app assets upon app startup
+        Self { dark_mode: true, 
+                assets
+            }
+    }
+
+    // This function is implemented so all assets used in the app will be loaded in upon app startup and never need to be loaded up again, saving GPU resources and increasing processing speed
+    fn load_assets_from_bytes(ctx: &egui::Context) -> Vec<TextureHandle> {
+
+        let asset_path = vec![
+            ("moon", include_bytes!("../assets/Moon_Crescent.png"))
+            // ("sun", include_bytes!("Add sun path when its added to assets"))
+        ];
+
+        asset_path
+        .into_iter() // Do for each asset
+        .map(|(name, bytes)| {
+
+            let image = image::load_from_memory(bytes).expect("Failed to load image"); // Load the image from memory or return an error message should this fail
+            let size = image.dimensions(); // Size the image
+            let rgba = image.to_rgba8(); // Saves Image Colour Data
+            let pixels = rgba.as_flat_samples(); // Saves Pixel and MetaData.
+
+            // Create the asset for use in the app using raw pixel data
+            let color_image = egui::ColorImage::from_rgba_unmultiplied(
+                [size.0 as usize, size.1 as usize],
+                pixels.as_slice(),
+            );
+
+            // Load the asset into the app
+            ctx.load_texture(name, color_image, egui::TextureOptions::default())
+        })
+        .collect() // Once all iterations are complete, turn it back into a Collection (Vector)
     }
 }
+
 // Define the app's behvaiour and contents
 impl App for GameLog {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
@@ -30,12 +67,18 @@ impl App for GameLog {
                     // ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| { });
                     // ui.add_space(ui.available_width());
 
+                    
+
+                let dark_texture = &self.assets[0];
+                let size = egui::Vec2::new(35.0, 35.0);
+
+                // Create a SizedTexture from TextureHandle and size
+                let sized_dark_texture = egui::Image::new(dark_texture).max_size(size);
+
                 // Dark mode toggle is an exception and is set to the end of the top bar at all times. This is because it will be familiar for users as many other websites do this
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                    let label = if self.dark_mode { "Light Mode" } else { "Dark Mode" };
 
-                    if ui.add(egui::Button::new(label)
-                        .min_size(Vec2::new(100.0, 50.0 )))
+                    if ui.add( egui::ImageButton::new(sized_dark_texture))
                     .clicked() {
                         self.dark_mode = !self.dark_mode;
                     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod app_setup;
+pub mod app_setup;
 use eframe::{run_native, App, NativeOptions};
 use crate::app_setup::GameLog;
 
@@ -6,8 +6,8 @@ fn main() -> Result<(), eframe::Error> {
     let native_options = NativeOptions::default(); // Can be edited to change the windows display options (Size and VSync for example but we dont need that for my app)
 
     // Uses a closure '| |' to execute a function which creates the app window when called. This is basically a lamda. Here 'cc' is the parameter and can be used to configure the app on startup (Persist Storage, Light/Dark mode, etc.))
-    let app_creator = Box::new(|_cc: &eframe::CreationContext|  { 
-            Ok(Box::new(GameLog {dark_mode: true}) as Box<dyn App>) // Creates the app through creating a window using the GameLog data 
+    let app_creator = Box::new(|cc: &eframe::CreationContext|  { 
+            Ok(Box::new(GameLog::startup(&cc.egui_ctx)) as Box<dyn App>) // Creates the app through creating a window using the GameLog data 
         }
     );
 
