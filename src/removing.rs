75903d1e6d3f3669dd9b5c59db46ae45802b7f81
Darkoh <lukekirk05@gmail.com>
2025-07-14 04:31:42 +0100

    Added Checkbox and Button (no functionality yet)

--- src/removing.rs
+++ src/removing.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{Align, Label, Layout, RichText, TextEdit, Vec2};
+use eframe::egui::{Align, Button, Checkbox, Label, Layout, RichText, TextEdit, Vec2};
 
 use crate::{app_setup::GameLog, egui::Ui};
 
@@ -6,30 +6,62 @@ use crate::{app_setup::GameLog, egui::Ui};
 impl GameLog {
     pub fn removing_gui (&mut self, ui: &mut Ui) {
 
-        let label_size = Vec2::new(100.0, 20.0);
+        ui.set_max_size(Vec2::new(200.0, 50.0)); // Set to match element sizes for a perfect layout
+
+        let label_size = Vec2::new(ui.available_width(), 20.0); // Should use available width on the left most element (in this case the label) as this will center them
         let input_box_size = Vec2::new(150.0, 20.0);
-        let container_height = label_size.y + input_box_size.y + 20.0; // Width to hold elements being held in a horizontal container (Updates with each Layout to match the new space needed)
-        ui.add_space(5.0);
+        let button_size = Vec2::new(100.0, 20.0);
 
+        self.enabled = self.checked;
+
+        // Enter Game Name
         ui.vertical_centered(|ui| {
-            ui.group(|ui| {
-                ui.set_height(container_height); // constrain group width to input box width
-                ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
+            ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
 
-                    ui.add_sized(label_size,
-                    Label::new(RichText::new("Game Name").strong())
-                    );
+                ui.add_sized(
+                    label_size,
+                    Label::new(RichText::new("Game Name")
+                        .strong())
+                );
 
-                    ui.add_sized(
-                    input_box_size,
-                    TextEdit::singleline(&mut self.remove_game_name)
+                ui.add_sized(
+                input_box_size,
+                TextEdit::singleline(&mut self.remove_game_name)
                     .hint_text("Game Name")
                     .horizontal_align(Align::Center)
-                    .char_limit(50));
+                    .char_limit(50)
+                );
 
-                    ui.add_space(5.0);
-                });
+                ui.add_space(20.0);
             });
         });
+
+        ui.add_space(20.0);
+
+        // Confirm Removal Check + Button
+        ui.vertical_centered(|ui| {
+            ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
+                ui.add_sized(
+                    label_size,
+                    Checkbox::new(
+                        &mut self.checked,
+                        "Are you sure you wish to Remove this Game?",
+                    ),
+                );
+
+                ui.add_space(10.0);
+
+                ui.add_enabled_ui(self.enabled, |ui| {
+                    if ui.add_sized(Vec2::new(150.0, 20.0), Button::new("Go"))
+                        .clicked() {
+                             // Code Here to remove a Game
+                        }
+                });
+
+                ui.add_space(5.0);
+
+            });
+        });
+        ui.add_space(5.0);
     }
 }
